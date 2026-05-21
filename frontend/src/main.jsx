6421ec0e505efd101f7f93d4cdc0d0a26eed5733
Alex Bogdan <proyecto.empresa.bogdan@gmail.com>
2026-05-21 20:35:21 +0200

    fix: Use HashRouter to prevent Nginx 404 on direct navigation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AdminApp from './AdminApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<AdminApp />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
